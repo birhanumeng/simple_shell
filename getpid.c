05d263b3400f754b92355ef246bf12795737ea47
birhanumeng <eyerusalemmengiste@gmail.com>
2022-09-07 05:04:46 -0700

    getpid

---
+++ getpid.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	unsigned int pid = getpid();
+
+	printf("Proccess id: %d\n", pid);
+
+	return(0);
+}
